6b8760c9fa7a5d8c15ba7f3e6f516eb16f0ab0cf
BeamanN <BeamanNegash@gmail.com>
2025-03-22 14:36:24 -0400

    Saving project to GitHub

---
+++ config.py
@@ -0,0 +1,13 @@
+API_KEY = "PKYZ19WKHH7MVHQC99A2"
+SECRET_KEY = "7pDxvWf8MOwqad0VyyMNoOqebaHEUmzARpMzcFEC"
+
+ALPACA_CONFIG = {
+# Put your own Alpaca key here:
+"API_KEY": "PKYZ19WKHH7MVHQC99A2",
+# Put your own Alpaca secret here:
+"API_SECRET": "7pDxvWf8MOwqad0VyyMNoOqebaHEUmzARpMzcFEC",
+# If you want to go live, you must change this
+"ENDPOINT": "https://paper-api.alpaca.markets"
+}
+
+
